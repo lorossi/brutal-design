fd3cdcdfd3b0bb54c24790193b5d46048840fe68
Lorenzo Rossi <mail@lorenzoros.si>
2023-09-26 18:45:14 +0200

    added palettes

---
+++ js/palette.js
@@ -0,0 +1,29 @@
+class Palette {
+  constructor(foreground, background) {
+    this._foreground = foreground;
+    this._background = background;
+  }
+
+  get foreground() {
+    return this._foreground;
+  }
+
+  get background() {
+    return this._background;
+  }
+}
+
+const PALETTES = [
+  new Palette("#1e211e", "#89fc00"),
+  new Palette("#1e211e", "#dc0073"),
+  new Palette("#1e211e", "#008bf8"),
+  new Palette("#1e211e", "#deff0a"),
+];
+
+class PaletteFactory {
+  static random() {
+    return PALETTES[Math.floor(Math.random() * PALETTES.length)];
+  }
+}
+
+export { Palette, PaletteFactory };
--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,6 @@
-import { Engine, SimplexNoise, Point, Color } from "./engine.js";
-import { XOR128 } from "./xor128.js";
+import { Engine, SimplexNoise } from "./engine.js";
 import { Partition } from "./partition.js";
+import { Palette, PaletteFactory } from "./palette.js";
 
 class Sketch extends Engine {
   preload() {
@@ -8,19 +8,22 @@ class Sketch extends Engine {
   }
 
   setup() {
-    this._background = "#2be831";
-    this._foreground = "#1b1301";
+    this._palette = PaletteFactory.random();
+    console.log(this._palette);
     this._scl = 0.95;
     this._font = "VioletSans";
 
     this._partition = new Partition(0, 0, this.width, 1);
-    this._partition.setColors(this._background, this._foreground);
+    this._partition.setColors(
+      this._palette.background,
+      this._palette.foreground
+    );
     this._partition.split();
   }
 
   draw() {
     this.ctx.save();
-    this.background(this._background);
+    this.background(this._palette.background);
     this.ctx.translate(this.width / 2, this.height / 2);
     this.ctx.scale(this._scl, this._scl);
     this.ctx.translate(-this.width / 2, -this.height / 2);
@@ -35,19 +38,36 @@ class Sketch extends Engine {
   }
 
   _createTexture() {
-    // load the texture from file
-    this._texture = new Image();
-    this._texture.src = "assets/texture.jpg";
+    this._texture = document.createElement("canvas");
+    this._texture.width = this.width;
+    this._texture.height = this.height;
+
+    const ctx = this._texture.getContext("2d");
+    const noise = new SimplexNoise();
+    noise.setDetail(5, 0.25);
+    const n_scl = 0.01;
+    const scl = 2;
+
+    // draw background
+    for (let x = 0; x < this.width; x += scl) {
+      for (let y = 0; y < this.height; y += scl) {
+        const n = noise.noise(x * n_scl, y * n_scl);
+        const c = Math.floor((64 * (n + 1)) / 2);
+        ctx.fillStyle = `rgba(${c}, ${c}, ${c}, 0.1)`;
+        ctx.fillRect(x, y, scl, scl);
+      }
+    }
   }
 
   _addTexture() {
     this.ctx.save();
-    this.ctx.globalCompositeOperation = "darker";
-    this.ctx.filter = "invert(1)";
-    this.ctx.globalAlpha = 0.25;
+    this.ctx.globalCompositeOperation = "lighter";
+
     this.ctx.translate(this.width / 2, this.height / 2);
     this.ctx.rotate(Math.floor(Math.random() * 4) * (Math.PI / 2));
-    this.ctx.drawImage(this._texture, 0, 0, this.width, this.height);
+    this.ctx.translate(-this.width / 2, -this.height / 2);
+
+    this.ctx.drawImage(this._texture, 0, 0);
 
     this.ctx.restore();
   }
@@ -57,12 +77,12 @@ class Sketch extends Engine {
 
     const height = Math.floor(biggest.size / 10);
 
-    this.ctx.fillStyle = this._background;
+    this.ctx.fillStyle = this._palette.background;
     this.ctx.font = `bold ${height}px ${this._font}`;
     this.ctx.textAlign = "left";
     this.ctx.textBaseline = "top";
 
-    const text = "GRAPHIC DESIGN";
+    const text = "BRUTAL DESIGN";
 
     this.ctx.translate(biggest.border * 2, biggest.border * 2);
 
