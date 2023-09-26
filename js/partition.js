5ef4dee59525205d91f4df93e9e9fd59d95e277d
Lorenzo Rossi <mail@lorenzoros.si>
2023-09-26 18:22:11 +0200

    initial commit

---
+++ js/partition.js
@@ -0,0 +1,111 @@
+class Partition {
+  constructor(x, y, size, probability, level = 0) {
+    this._x = x;
+    this._y = y;
+    this._size = size;
+    this._probability = probability;
+    this._level = level;
+
+    this._children = [];
+    this._border = 8;
+    this._rotation = ((Math.random() * 2 - 1) * Math.PI) / 180;
+    this._cols = 2;
+  }
+
+  setColors(background, foreground) {
+    this._background = background;
+    this._foreground = foreground;
+  }
+
+  split() {
+    this._children = [];
+
+    if (this._size < 4 * this._border) return;
+    if (Math.random() ** 2 > this._probability) return;
+
+    const n_size = this._size / this._cols;
+
+    // leave one empty if level is 0
+    const empty = Math.floor(Math.random() * this._cols ** 2);
+
+    for (let i = 0; i < this._cols * this._cols; i++) {
+      const x = this._x + (i % this._cols) * n_size;
+      const y = this._y + Math.floor(i / this._cols) * n_size;
+
+      let probability;
+      if (this._level == 0) {
+        if (i == empty) probability = 0;
+        else probability = 1;
+      } else probability = this._probability * 0.5;
+
+      const p = new Partition(
+        x,
+        y,
+        this._size / 2,
+        probability,
+        this._level + 1
+      );
+
+      p.setColors(this._background, this._foreground);
+      p.split();
+      this._children.push(p);
+    }
+  }
+
+  _draw(ctx) {
+    ctx.save();
+
+    ctx.fillStyle = this._foreground;
+    ctx.strokeStyle = this._background;
+    ctx.lineWidth = this._border;
+
+    ctx.translate(this._x + this._size / 2, this._y + this._size / 2);
+    ctx.rotate(this._rotation);
+
+    ctx.beginPath();
+    ctx.rect(-this._size / 2, -this._size / 2, this._size, this._size);
+    ctx.fill();
+    ctx.stroke();
+    ctx.restore();
+  }
+
+  show(ctx) {
+    if (this._children.length > 0) this._children.forEach((c) => c.show(ctx));
+    else this._draw(ctx);
+  }
+
+  biggestEmptyChild() {
+    // non recursive function to find the biggest empty child of a partition
+    if (this._children.length == 0) return this;
+
+    let biggest = null;
+    let biggest_size = 0;
+    for (let i = 0; i < this._children.length; i++) {
+      const child = this._children[i];
+      if (child._children.length == 0 && child._size > biggest_size) {
+        biggest = child;
+        biggest_size = child._size;
+      }
+    }
+
+    return biggest;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  get y() {
+    return this._y;
+  }
+
+  get border() {
+    return this._border;
+  }
+}
+
+export { Partition };
--- js/sketch.js
+++ js/sketch.js
@@ -1,22 +1,85 @@
-/*
-  HTML canvas sketch. GitHub repo and some basic documentation: https://github.com/lorossi/empty-html5-canvas-project
-  Made by Lorenzo Rossi. Website and contacts: https://lorenzoros.si/
-*/
-
 import { Engine, SimplexNoise, Point, Color } from "./engine.js";
 import { XOR128 } from "./xor128.js";
+import { Partition } from "./partition.js";
 
 class Sketch extends Engine {
   preload() {
-    // ran once. Ideally, this has never to be called again
+    this._createTexture();
   }
 
   setup() {
-    // ran once. This can be called multiple times
+    this._background = "#2be831";
+    this._foreground = "#1b1301";
+    this._scl = 0.95;
+    this._font = "VioletSans";
+
+    this._partition = new Partition(0, 0, this.width, 1);
+    this._partition.setColors(this._background, this._foreground);
+    this._partition.split();
   }
 
   draw() {
-    // looping continuously at a set framerate
+    this.ctx.save();
+    this.background(this._background);
+    this.ctx.translate(this.width / 2, this.height / 2);
+    this.ctx.scale(this._scl, this._scl);
+    this.ctx.translate(-this.width / 2, -this.height / 2);
+    this._partition.show(this.ctx);
+    const biggest = this._partition.biggestEmptyChild();
+    this._drawText(biggest);
+    this.ctx.restore();
+
+    this._addTexture();
+
+    this.noLoop();
+  }
+
+  _createTexture() {
+    // load the texture from file
+    this._texture = new Image();
+    this._texture.src = "assets/texture.jpg";
+  }
+
+  _addTexture() {
+    this.ctx.save();
+    this.ctx.globalCompositeOperation = "darker";
+    this.ctx.filter = "invert(1)";
+    this.ctx.globalAlpha = 0.25;
+    this.ctx.translate(this.width / 2, this.height / 2);
+    this.ctx.rotate(Math.floor(Math.random() * 4) * (Math.PI / 2));
+    this.ctx.drawImage(this._texture, 0, 0, this.width, this.height);
+
+    this.ctx.restore();
+  }
+
+  _drawText(biggest) {
+    this.ctx.save();
+
+    const height = Math.floor(biggest.size / 10);
+
+    this.ctx.fillStyle = this._background;
+    this.ctx.font = `bold ${height}px ${this._font}`;
+    this.ctx.textAlign = "left";
+    this.ctx.textBaseline = "top";
+
+    const text = "GRAPHIC DESIGN";
+
+    this.ctx.translate(biggest.border * 2, biggest.border * 2);
+
+    const replicas = 5;
+    for (let i = 0; i < replicas; i++) {
+      this.ctx.save();
+      this.ctx.translate(0, i * height * 0.85);
+      this.ctx.fillText(text, biggest.x, biggest.y);
+      this.ctx.restore();
+    }
+
+    this.ctx.restore();
+  }
+
+  click() {
+    this.setup();
+    this.draw();
   }
 }
 
